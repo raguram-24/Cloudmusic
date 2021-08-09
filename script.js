5a42d40f79a5c8c2e669655a044092bd7dde4ff1
raguram-24 <ram.ragu2404@gmail.com>
2021-08-09 11:35:20 +0530

    CloudMusic

---
+++ script.js
@@ -0,0 +1,22 @@
+SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+  $(document).ready(function() {  
+    SC.stream('/tracks/506673903',function(sound){
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+ 
+
+     });
+
+
+
+  }); 
+ 
